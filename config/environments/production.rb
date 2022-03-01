11a4705412ebf810b8b6466bf1da8153592e07a0
Igor Klein <igorklein1988@gmail.com>
2022-03-01 13:05:35 +0100

    add mail server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,31 +120,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'taskm2022@gmail.com',
-    password: 'zjyihthqgmnmjlzc',
-    address: 'smtp.gmail.com',
-    port: 587,
-    authentication: 'plain',
+    user_name: ENV['MAILER_USERNAME'],
+    password: ENV['MAILER_PASSWORD'],
+    address: ENV['MAILER_ADDRESS'],
+    port: ENV['MAILER_PORT'],
+    authentication: ENV['MAILER_AUTHENTICATION'],
     enable_starttls_auto: true,
   }
-
-  # config.action_mailer.smtp_settings = {
-  # address:              'smtp.gmail.com',
-  # port:                 587,
-  # domain:               'example.com',
-  # user_name:            '<username>',
-  # password:             '<password>',
-  # authentication:       'plain',
-  # enable_starttls_auto: true }
-
-  # 
-  # config.action_mailer.smtp_settings = {
-  #   user_name: ENV['MAILER_USERNAME'],
-  #   password: ENV['MAILER_PASSWORD'], oxsbblghvqoawyzu
-  #   address: ENV['MAILER_ADDRESS'],
-  #   port: ENV['MAILER_PORT'],
-  #   domain: ENV['MAILER_DOMAIN'],
-  #   authentication: ENV['MAILER_AUTHENTICATION'],
-  #   enable_starttls_auto: true,
-  # }
 end
